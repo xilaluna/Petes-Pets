db839a18b8a7e81713e5aaaf98f5cb8a4a21d6ef
Amyavow <amy.avow@gmail.com>
2021-05-31 16:18:44 +0100

    fix deprecation warning issue with mongoose.connect() options

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const methodOverride = require('method-override')
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/petes-pets');
+mongoose.connect('mongodb://localhost/local', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
