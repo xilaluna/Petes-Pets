73f0eca8b123557e939d92d117105a268257c683
Xila Luna <xilaluna2@gmail.com>
2021-09-01 21:32:49 -0700

    cloned starter and added comments

--- server.js
+++ server.js
@@ -1,62 +1,61 @@
 if (!process.env.PORT) {
-  require('dotenv').config()
+  require("dotenv").config()
   process.env.NODE_ENV = "dev"
 }
 
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+const express = require("express")
+const path = require("path")
+const favicon = require("serve-favicon")
+const logger = require("morgan")
+const cookieParser = require("cookie-parser")
+const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
 
-const app = express();
+const app = express()
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/local', {
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/local", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
-});
+  useFindAndModify: false,
+})
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug") // This itself is a middle ware
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")))
 
 // override with POST having ?_method=DELETE or ?_method=PUT
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(logger("dev")) // this is a middleware that allows for easy logging in the terminal
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()) // body parser is required for sumbitting forms.
+app.use(cookieParser()) //  This is also a middleware.
 
-
-require('./routes/index.js')(app);
-require('./routes/pets.js')(app);
+require("./routes/index.js")(app)
+require("./routes/pets.js")(app)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error("Not Found")
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render("error")
+})
 
-module.exports = app;
+module.exports = app
