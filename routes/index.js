34c72f1ee0dfd8dc98d9cb46f31bd87bec09c6e8
Xila Luna <xilaluna2@gmail.com>
2021-09-05 20:26:05 -0700

    Implemented pagination on the home page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const Pet = require('../models/pet');
+const Pet = require("../models/pet")
 
 module.exports = (app) => {
-
   /* GET home page. */
-  app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
-    });
-  });
+  app.get("/", (req, res) => {
+    const page = req.query.page || 1
+
+    Pet.paginate({}, { page: page }).then((results) => {
+      res.render("pets-index", { pets: results.docs, pagesCount: results.pages, currentPage: page })
+    })
+  })
 }
