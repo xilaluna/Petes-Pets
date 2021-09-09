01b5b634f75b876796b2860322d0e003dc4a25a3
Xila Luna <xilaluna2@gmail.com>
2021-09-09 12:14:57 -0700

    Implemented S3 file uploads

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,15 +1,17 @@
 if (document.querySelector("#new-pet")) {
   document.querySelector("#new-pet").addEventListener("submit", (e) => {
     e.preventDefault()
+    // Use FormData to grab everything now that we have files mixed in with text
+    var form = document.getElementById("new-pet")
+    var pet = new FormData(form)
 
-    let pet = {}
-    const inputs = document.querySelectorAll(".form-control")
-    for (const input of inputs) {
-      pet[input.name] = input.value
-    }
-
+    // Assign the multipart/form-data headers to axios does a proper post
     axios
-      .post("/pets", pet)
+      .post("/pets", pet, {
+        headers: {
+          "Content-Type": "multipart/form-data;",
+        },
+      })
       .then(function (response) {
         window.location.replace(`/pets/${response.data.pet._id}`)
       })
