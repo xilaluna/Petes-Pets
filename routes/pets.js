1462a0c2eaf10384eea457642521cac3975b85f9
Xila Luna <xilaluna2@gmail.com>
2021-09-12 22:37:13 -0700

    Implemented Emails

--- routes/pets.js
+++ routes/pets.js
@@ -32,6 +32,8 @@ const client = new Upload(process.env.S3_BUCKET, {
 // MODELS
 const Pet = require("../models/pet")
 
+const mailer = require("../utils/mailer")
+
 // PET ROUTES
 module.exports = (app) => {
   // INDEX PET => index.js
@@ -151,6 +153,48 @@ module.exports = (app) => {
         })
         .catch((err) => {
           console.log("Error:" + err)
+        })
+    })
+  })
+
+  app.post("/pets/:id/purchase", (req, res) => {
+    console.log(req.body)
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here: https://dashboard.stripe.com/account/apikeys
+    var stripe = require("stripe")(process.env.PRIVATE_STRIPE_API_KEY)
+
+    // Token is created using Checkout or Elements!
+    // Get the payment token ID submitted by the form:
+    const token = req.body.stripeToken // Using Express
+
+    // req.body.petId can become null through seeding,
+    // this way we'll insure we use a non-null value
+    let petId = req.body.petId || req.params.id
+
+    Pet.findById(petId).exec((err, pet) => {
+      if (err) {
+        console.log("Error: " + err)
+        res.redirect(`/pets/${req.params.id}`)
+      }
+      const charge = stripe.charges
+        .create({
+          amount: pet.price * 100,
+          currency: "usd",
+          description: `Purchased ${pet.name}, ${pet.species}`,
+          source: token,
+        })
+        .then((chg) => {
+          // Convert the amount back to dollars for ease in displaying in the template
+          const user = {
+            email: req.body.stripeEmail,
+            amount: chg.amount / 100,
+            petName: pet.name,
+          }
+          // Call our mail handler to manage sending emails
+          mailer.sendMail(user, req, res)
+        })
+        .catch((err) => {
+          console.log("Error: " + err)
         })
     })
   })
